746ae497d46e7865dc9ddafd44560c56525345d7
Vadim Axelrod <vadim@vadim.com>
2017-01-26 15:40:01 -0500

    Adding model and migration

--- area_app/models.py
+++ area_app/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+
+
+class Problem(models.Model):
+    user = models.ForeignKey(User)
+    decision_type = models.CharField(max_length=255)
+    decision = models.CharField(max_length=255)
+    options = models.CharField(max_length=255)
+    time_frame = models.CharField(max_length=255)
+    decision_type_other = models.CharField(max_length=255)
+    success = models.CharField(max_length=255)
+
+
+class Question(models.Model):
+    problem = models.ForeignKey(Problem)
+    answer = models.CharField(max_length=50)
