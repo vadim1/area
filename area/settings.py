329d0815fa5b448f2de476b076a0addd091d7139
Vadim Axelrod <vadimaxelrod@Vadims-MacBook-Pro.local>
2017-03-25 11:48:30 -0400

    Adding static dir

--- area/settings.py
+++ area/settings.py
@@ -149,7 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, "static"), '/home/ubuntu/area/area_app/static/area_app'] # TODO - finish config
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static"),
+                   '/home/ubuntu/area/area_app/static/area_app',
+                   '/var/www/static/',]  # TODO - finish config
 
 try:
     from local_settings import *
