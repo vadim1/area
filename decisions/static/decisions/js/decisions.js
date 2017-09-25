a7a35f4adedda491f3b8098ed48e292f66054a71
Vadim Axelrod <vadimaxelrod@Vadims-MBP-2.fios-router.home>
2017-09-25 12:57:53 -0400

    Updates per discussion / call

--- decisions/static/decisions/js/decisions.js
+++ decisions/static/decisions/js/decisions.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    $(".start").delay(500).fadeTo("slow", 1.0);
+$(document).ready(function () {
+    $(".start").delay(1000).fadeTo("slow", 1.0);
+    $(".start2").delay(2000).fadeTo("slow", 1.0);
 });
+
+function nextbtn() {
+    var first_hidden = $('#list').find('li:hidden').first();
+    first_hidden.show();
+    if ($('#list').find('li:hidden').length) {
+        // There are more hidden items
+        $('#and').appendTo(first_hidden);
+    } else {
+        $('#and').hide();
+    }
+    return false;
+}
