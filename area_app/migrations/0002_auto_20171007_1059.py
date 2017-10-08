58b64cc910109fb2d88a29ab18d70531e8248392
sonus21 <sonunitw12@gmail.com>
2017-10-08 10:05:57 +0530

    Changed migrations files.

--- area_app/migrations/0002_auto_20171007_1059.py
+++ area_app/migrations/0002_auto_20171007_1059.py
@@ -10,8 +10,12 @@ class Migration(migrations.Migration):
     dependencies = [
         ('area_app', '0001_initial'),
     ]
-
     operations = [
+        migrations.AddField(
+            model_name='user',
+            name='phone_number',
+            field=models.CharField(blank=True, max_length=16, null=True),
+        ),
         migrations.AddField(
             model_name='user',
             name='dream_director',
